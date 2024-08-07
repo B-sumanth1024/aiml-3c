4e26016c081daaa65b48de3e038cb523e7b1eec8
B-sumanth1024 <160298146+B-sumanth1024@users.noreply.github.com>
2024-08-07 18:05:32 +0530

    10. Print a Pattern.py

---
+++ 10. Print a Pattern.py
@@ -0,0 +1,11 @@
+rows = int(input("Enter the number of rows: "))
+for row in range(1, rows + 1):
+    for col in range(row):
+        print('*', end='')
+    print()
+
+Or
+
+rows = int(input("Enter the number of rows: "))
+for i in range(1, rows + 1):
+    print('*' * i)
